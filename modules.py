db81a708f067985e2352b7da146ed92a06d05f6b
Larry Zhou <larryzms@gmail.com>
2019-04-16 20:49:52 -0400

    init

---
+++ modules.py
@@ -0,0 +1,106 @@
+
+
+
+class Indexer():
+
+	def __init__(self):
+		pass
+
+	def do_index(self):
+		#loop thru corpus directory
+		pass
+
+	def index_single_doc(self):
+		#index a single document
+		#index includes: tf, position
+		pass
+
+	def close_indexer(self):
+		#dump_files
+		pass
+
+
+
+class Retrival():
+
+	def __init__(self):
+		pass
+
+	def load_index(self):
+		#load index into memory
+		pass
+
+	def search(self):
+		#do search on a query
+		pass
+
+	def BM25(self):
+		pass
+
+	def query_lilekihood(self):
+		pass
+
+	def tf_idf(self):
+		pass
+
+	def query_time_stemming(self):
+		pass
+
+	def sudo_relevance(self):
+		pass
+
+	def snippet_generation(self):
+		pass
+
+	def query_highlighting(self):
+		pass
+
+	def kl_diverge(self):
+		pass
+
+
+
+def Eval():
+
+	def __init__(self):
+		pass
+
+
+	def MAP(self):
+		pass
+
+	def MRR(self):
+		pass
+
+	def pos_at_k(self):
+		pass
+
+	def precision_recall(self):
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
